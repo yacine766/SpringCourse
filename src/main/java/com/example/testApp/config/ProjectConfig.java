c9da5d4d36c19c71164d815031d38aaf8d4d26b4
ybensaid <yacine76@gmail.com>
2022-11-16 19:40:32 +0100

    bean with component

--- src/main/java/com/example/testApp/config/ProjectConfig.java
+++ src/main/java/com/example/testApp/config/ProjectConfig.java
@@ -2,35 +2,12 @@ package com.example.testApp.config;
 
 import com.example.testApp.bean.Vehicle;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "package com.example.testApp.bean")
 public class ProjectConfig {
 
-    @Bean(value="audiVehicle")
-    Vehicle vehicle1 (){
-        var veh = new Vehicle();
-        veh.setName("Audi");
-        return veh;
-    }
-    @Bean(value="mercedesVehicle")
-    Vehicle vehicle2 (){
-        var veh = new Vehicle();
-        veh.setName("mercedes");
-        return veh;
-    }
-
-    @Bean
-    String hello(){
-        return "hello ,i m bean hello";
-    }
-    @Bean
-    String saludos(){
-        return "saludos";
-    }
-
-    @Bean
-    int number(){
-        return 16;
-    }
 }
